5720af37f379ee81ce8bb4e7a2f02ae3c0080770
Victor Ruelas-Rivera <ruelasvh@gmail.com>
2018-03-03 15:58:16 -0500

    Can copy GIF to clipboard

--- src/components/GifItem.js
+++ src/components/GifItem.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import '../styles/GifItem.css'
 
-const Gif = ({ url }) => (
-    <img src={url} className="gif hidden item" alt="gif"/>
-)
+class Gif extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            copiedUrl: false
+        }
+
+        this.handleCopy = this.handleCopy.bind(this)
+    }
+
+    handleCopy (event) {
+        let textField = document.createElement('textarea')
+        textField.innerText = this.props.url
+        document.body.appendChild(textField)
+        textField.select()
+        this.setState({copiedUrl: document.execCommand('copy')})
+        textField.remove()
+        setTimeout(() => this.setState({copiedUrl: false}), 4000)
+    }
+
+    render () {
+        const { url } = this.props
+
+        return (
+            <div className="gif-container">
+                <img src={url} className="gif-image" alt="gif"/>
+                <div id="alert-box" className={this.state.copiedUrl ? 'slide-down': ''}>
+                    Copied to clipboard!
+                </div>
+                <div className="gif-middle" onClick={this.handleCopy}>
+                    <i className="fa fa-link fa-2x gif-text"></i>
+                </div>
+            </div>
+        )
+    }
+}
+
 
 
 Gif.propTypes = {
